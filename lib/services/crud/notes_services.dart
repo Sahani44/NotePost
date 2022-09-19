f26db941e51cec5c3cc13ef31c02bf0989fded8c
Devansh_Sahani <sahanidev21@gmail.com>
2022-09-20 00:00:39 +0530

    crud funs to database

---
+++ lib/services/crud/notes_services.dart
@@ -0,0 +1,128 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart'
+    show join;
+
+
+class DatabaseAlreadyOpenException implements Exception{}
+
+class UnableToGetDocumentsDirectory implements Exception{}
+
+class DatabaseIsNotOpen implements Exception{}
+
+class NotesServices {
+
+  Database? _db;
+
+  Future<void> open() async {
+    if( _db != null ) {
+      throw DatabaseAlreadyOpenException();
+    }
+    try {
+      final docsPath = await getApplicationDocumentsDirectory();
+      final dbPath = join(docsPath.path, dbName);
+      final db = await openDatabase(dbPath);
+      _db = db;
+
+      // create the user table
+
+      await db.execute(createUserTable);
+
+      //create notes table 
+
+      await db.execute(createNoteTable);
+    } on MissingPlatformDirectoryException {
+        throw UnableToGetDocumentsDirectory(); 
+    }
+  }
+
+  Future<void> close() async {
+
+  final db = _db;
+  if(db == null) {
+    throw DatabaseIsNotOpen();
+  } else {
+    await db.close(); 
+    _db = null;
+  }
+
+}
+
+}
+
+
+
+
+
+
+@immutable
+class DatabaseUser {
+
+  final int id;
+  final String email;
+
+  const DatabaseUser({required this.id, required this.email});
+
+  DatabaseUser.fromRow(Map<String, Object?> map) : id = map[idColumn] as int , email = map[emailColumn] as String;
+
+  @override
+  String toString() => 'Person , ID = $id, email = $email';
+
+  @override bool operator == (covariant DatabaseUser other) => id == other.id;
+  
+  @override
+  int get hashCode => id.hashCode;
+  
+
+}
+
+
+class DatabaseNotes {
+
+  final int id;
+  final int userId;
+  final String text;
+  final bool isSyncedWithCloud;
+
+  DatabaseNotes({required this.id, required this.userId, required this.text, required this.isSyncedWithCloud});
+
+  DatabaseNotes.fromRow(Map<String, Object?> map)  
+      : id = map[idColumn] as int , 
+        userId = map[userIdColumn] as int,
+        text = map[textColumn] as String,
+        isSyncedWithCloud = (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
+
+  @override
+  String toString() => 'Note, ID = $id, userId = $userId, isSyncedWithCloud = $isSyncedWithCloud, text = $text';
+
+  @override bool operator == (covariant DatabaseUser other) => id == other.id;
+  
+  @override
+  int get hashCode => id.hashCode;
+
+}
+
+const dbName = 'notes.db';
+const noteTable = 'note';
+const userTable = 'user'; 
+const idColumn = 'id';
+const emailColumn = 'email';
+const userIdColumn = 'user_id';
+const textColumn = 'text';
+const isSyncedWithCloudColumn = 'is_synced_with_cloud';
+const createNoteTable = '''CREATE TABLE IF NOT EXISTS "note" (
+	"id"	INTEGER NOT NULL,
+	"user_id"	INTEGER NOT NULL,
+	"text"	TEXT,
+	"is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+	FOREIGN KEY("user_id") REFERENCES "user"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);''';
+const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
+	"id"	INTEGER NOT NULL,
+	"email"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);''';
